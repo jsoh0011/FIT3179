87eef2127d71beb8f017dc9d977706a0ca8762f3
jsoh0011 <102638980+jsoh0011@users.noreply.github.com>
2023-09-21 23:14:38 +0800

    Delete vis2_map.js

--- vis2_map.js
+++
@@ -1,5 +0,0 @@
-  var map = "/homework_wk9/js/vis2_map.vg.json";
-
-  vegaEmbed('#map', map).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-  }).catch(console.error);
