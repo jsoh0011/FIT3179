48c0f276b29a25d5a37efbeb6556a2ff9e7b7af0
jsoh0011 <102638980+jsoh0011@users.noreply.github.com>
2023-10-06 22:13:58 +0800

    Delete homework_wk10 directory

--- homework_wk10/hw_wk10.js
+++
@@ -1,6 +0,0 @@
-  var vis = "hw_wk10.vg.json";
-
-  vegaEmbed('#vis', vis).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-  }).catch(console.error);
-
